2facbf01b7524bdd9ba28f9ad0acfb9619670447
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-10-28 10:31:29 +0300

    Add set settings default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { places$ } from './data/index';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/first';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,12 @@ export class AppComponent {
   public title: string = 'Weather';
   public places$: Observable<Place[]> = places$;
   public currentPlace: Place;
+  public constructor() {
+    const getFirstElem: Observable<Place[]> = this.places$.first();
+    getFirstElem.subscribe(
+      (ev: any): any => {
+        this.currentPlace = ev[0];
+      }
+    );
+  }
 }
